aea0b2368be33cd438f5d582ebe67550db73aaf5
violettebunny <52981973+violettebunny@users.noreply.github.com>
2021-09-30 15:51:13 -0400

    created the routers and created tables in the database

--- app.js
+++ app.js
@@ -1,22 +1,36 @@
 const express = require('express');
 const morgan = require('morgan');
-const sequelize = require('sequelize');
+const Sequelize = require('sequelize');
+const { db, Page, User } = require('./models/index');
+const wikiRouter = require('./routes/wiki');
+// const userRouter = require('./routes/users');
 
 const app = express();
 
-app.use(morgan('dev'))
+app.use(morgan('dev'));
+app.use(
+  express.urlencoded({
+    extended: false,
+  })
+);
 
-app.use(express.urlencoded({
-    extended: false
-}))
+db.authenticate().then(() => {
+  console.log('Connected to the database');
+});
+
+app.use('/wiki', wikiRouter);
+// app.use('/user', userRouter);
 
 app.get('/', (req, res) => {
-    res.send('Hello')
-})
+  res.send('Hello');
+});
 
-const PORT = 8080;
+const init = async () => {
+  await db.sync({ force: false });
+  const PORT = 8080;
+  app.listen(PORT, () => {
+    console.log(`App is listening to port ${PORT}`);
+  });
+};
 
-app.listen(PORT, () => {
-  console.log(`App is listening to port ${PORT}`);
-})
-
+init();
--- models/index.js
+++ models/index.js
@@ -1,6 +1,40 @@
-const Sequlize = require('sequelize');
-const db = new Sequelize('postgress://localhost:5432/wikistack');
+const Sequelize = require('sequelize');
+const db = new Sequelize('postgres://localhost:5432/wikistack');
+
+const Page = db.define('Page', {
+  title: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  slug: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  content: {
+    type: Sequelize.TEXT,
+    allowNull: false,
+  },
+  status: {
+    type: Sequelize.ENUM('open', 'closed'),
+  },
+});
+
+const User = db.define('user', {
+  name: {
+    type: Sequelize.STRING,
+    allowNull: false,
+  },
+  email: {
+    type: Sequelize.STRING,
+    allowNull: false,
+    validate: {
+      isEmail: true,
+    },
+  },
+});
 
 module.exports = {
-    db
-}
+  db,
+  Page,
+  User,
+};
